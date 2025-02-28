e40c897f4a087dfff57dc0bbc56c494a6b091711
Queue-ri <qriositylog@gmail.com>
2025-02-28 13:31:39 +0900

    feat: 예약 신청 dto 추가 (#26)

---
+++ common-service/src/main/java/com/ssginc/commonservice/reserve/dto/ReserveRequestDto.java
@@ -0,0 +1,24 @@
+package com.ssginc.commonservice.reserve.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author Queue-ri
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ReserveRequestDto {
+    private Long entryId;
+    private Long memberCode;
+    private Long storeId;
+    private LocalDateTime reservedDateTime;
+    private Integer headcount;
+}
