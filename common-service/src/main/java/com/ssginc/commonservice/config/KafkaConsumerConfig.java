4551f7e0f203fd0417358b67fab99dd20c9d4570
Queue-ri <qriositylog@gmail.com>
2025-02-28 13:29:17 +0900

    chore(conf): Kafka config 추가 (#26)

--- common-service/src/main/java/com/ssginc/commonservice/config/KafkaConsumerConfig.java
+++ common-service/src/main/java/com/ssginc/commonservice/config/KafkaConsumerConfig.java
@@ -1,13 +1,12 @@
-package com.ssginc.reserveservice.config;
+package com.ssginc.commonservice.config;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
-import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
-import org.springframework.kafka.core.*;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 
 import java.util.HashMap;
 import java.util.Map;
