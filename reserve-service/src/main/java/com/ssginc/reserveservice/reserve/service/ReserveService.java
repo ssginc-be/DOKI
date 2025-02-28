16c0f0170b3b61ad38d6a8454ce0cc87b64bbae0
Queue-ri <qriositylog@gmail.com>
2025-02-28 13:38:34 +0900

    feat: 팝업스토어 예약 신청 `v2` API 추가 (#26)

---
+++ reserve-service/src/main/java/com/ssginc/reserveservice/reserve/service/ReserveService.java
@@ -0,0 +1,37 @@
+package com.ssginc.reserveservice.reserve.service;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssginc.reserveservice.exception.CustomException;
+import com.ssginc.reserveservice.exception.ErrorCode;
+import com.ssginc.reserveservice.reserve.dto.ReserveRequestDto;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author Queue-ri
+ */
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class ReserveService {
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+    private final ObjectMapper objectMapper;
+
+    public ResponseEntity<?> sendMessage(ReserveRequestDto dto) {
+        String message = null;
+        try {
+            message = objectMapper.writeValueAsString(dto);
+        } catch (Exception e) {
+            log.error("예약 요청 message 변환 실패");
+            throw new CustomException(ErrorCode.SOMETHING_WENT_WRONG);
+        }
+        log.info(message);
+        kafkaTemplate.send("doki-reserve", message);
+
+        return ResponseEntity.ok().build();
+    }
+}
