ca7d7094b0c122372772556a139ca908c81248fb
Queue-ri <qriositylog@gmail.com>
2025-02-28 18:20:11 +0900

    feat: [운영자] 예약 내역 승인/거절/취소 API 추가 (#30)

--- common-service/src/main/java/com/ssginc/commonservice/exception/ErrorCode.java
+++ common-service/src/main/java/com/ssginc/commonservice/exception/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode {
     STORE_NOT_FOUND(HttpStatus.NOT_FOUND, "DOKI-005", "팝업스토어를 찾을 수 없습니다."),
     RESERVATION_ALREADY_END(HttpStatus.FORBIDDEN, "DOKI-006", "예약이 마감되었습니다."),
     RESERVATION_EXISTS(HttpStatus.FORBIDDEN, "DOKI-007", "해당 일시의 예약 이력이 있습니다."),
+    RESERVATION_NOT_FOUND(HttpStatus.NOT_FOUND, "DOKI-008", "예약 이력을 찾을 수 없습니다."),
+    EXCEED_RESERVATION_CAPACITY(HttpStatus.FORBIDDEN, "DOKI-009", "예약 정원을 초과하여 승인할 수 없습니다."),
     SOMETHING_WENT_WRONG(HttpStatus.INTERNAL_SERVER_ERROR, "DOKI-099", "알 수 없는 오류가 발생했습니다.");
 
     private final HttpStatus httpStatus;	// http 상태 코드
--- common-service/src/main/java/com/ssginc/commonservice/store/controller/StoreRestControllerV1.java
+++ common-service/src/main/java/com/ssginc/commonservice/store/controller/StoreRestControllerV1.java
@@ -16,11 +16,32 @@ public class StoreRestControllerV1 {
     /*
         성능 테스트용 - 팝업스토어 목록 조회
     */
+    /*
+        예약 승인 / 거절 / 취소
+    */
     private final StoreService storeService;
 
     @GetMapping
     public ResponseEntity<?> getStoreList(@RequestParam(name="page", required=false) Integer pageIdx) {
         if (pageIdx == null) pageIdx = 0; // 루트 경로에서 호출 시 첫 페이지 조회
         return storeService.getStoreList(pageIdx);
+    }
+    
+    /* 예약 승인 */
+    @PutMapping("/reserve/confirm")
+    public ResponseEntity<?> confirmReservation(@RequestParam(name="id") Long rid) {
+        return storeService.confirmReservation(rid);
+    }
+
+    /* 예약 거절 */
+    @PutMapping("/reserve/refuse")
+    public ResponseEntity<?> refuseReservation(@RequestParam(name="id") Long rid) {
+        return storeService.refuseReservation(rid);
+    }
+
+    /* 예약 취소 */
+    @PutMapping("/reserve/cancel")
+    public ResponseEntity<?> cancelReservation(@RequestParam(name="id") Long rid) {
+        return storeService.cancelReservation(rid);
     }
 }
