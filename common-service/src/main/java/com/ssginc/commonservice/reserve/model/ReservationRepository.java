f176da12033fa2b199df0683e1787365c46d8d89
Queue-ri <qriositylog@gmail.com>
2025-02-28 13:37:39 +0900

    feat: 팝업스토어 예약 관련 엔티티 수정 (#26)

--- common-service/src/main/java/com/ssginc/commonservice/reserve/model/ReservationRepository.java
+++ common-service/src/main/java/com/ssginc/commonservice/reserve/model/ReservationRepository.java
@@ -1,10 +1,25 @@
 package com.ssginc.commonservice.reserve.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * @author Queue-ri
  */
 
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+
+    @Query(
+            "SELECT r " +
+            "FROM Reservation r " +
+            "WHERE r.reservationEntry.reservationEntryId = :entryId AND " +
+            "r.member.memberCode = :memberCode AND " +
+            "r.store.storeId = :storeId AND " +
+            "(r.reservationStatus = 'RESERVE_PENDING' OR r.reservationStatus = 'CONFIRMED')"
+    )
+    Optional<List<Reservation>> findPreviousReservation(@Param("entryId") Long entryId, @Param("memberCode") Long memberCode, @Param("storeId")Long storeId);
 }
