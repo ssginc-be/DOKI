5baa3cdb1188ab9033ff80b9e7646afa030ed47f
Queue-ri <qriositylog@gmail.com>
2025-03-05 16:48:33 +0900

    feat: 이용자 -> 운영자 SSE 알림 기본 UI 연결 (#32)
    알림을 수신할 시 알림 아이콘이 변경되고, 알림 개수가 업데이트 됨

--- common-service/src/main/resources/static/js/layout-manager.js
+++ common-service/src/main/resources/static/js/layout-manager.js
@@ -19,3 +19,48 @@ function gotoRoot() {
 function gotoStoreReservationPage() {
     location.href = "http://localhost:9093/store/reserve";
 }
+
+
+/*
+    SSE 알림
+*/
+let notiCount = 0; // 최초 페이지 로딩 시 초기 알림 개수
+const eventSource = new EventSource('http://localhost:9093/noti/subscribe');
+
+// SSE 최초 연결시
+eventSource.onopen = function() {
+    console.log('SSE 연결 성공');
+};
+
+// SSE 이벤트 발생시마다 --> 'message' 타입에만 동작하므로, 프로젝트에서 사용하지 않음
+// eventSource.onmessage = (event) => {
+//     const message = event.data;
+//     console.log('Received message:', message); // logging
+//
+//     // 알림 개수 증가
+//     notiCount += 1;
+//
+//     // 뷰 업데이트
+//     const alertBoxDiv = document.getElementById("navbar-alert-box"); // div
+//     const bellBoxImg = document.getElementById("navbar-bell-icon"); // img
+//
+//     alertBoxDiv.innerHTML = `확인하지 않은 알림이 ${notiCount}건 있습니다.`;
+//     bellBoxImg.src = "/icon/layout/bell_on_dark.svg";
+// };
+
+// SSE 이벤트 발생시마다 --> custom type 용
+// 운영자는 RESERVE_REQUEST type에 대한 이벤트만 수신함.
+eventSource.addEventListener("RESERVE_REQUEST", (event) => {
+    const message = event.data;
+    console.log('Received message:', message); // logging
+
+    // 알림 개수 증가
+    notiCount += 1;
+
+    // 뷰 업데이트
+    const alertBoxDiv = document.getElementById("navbar-alert-box"); // div
+    const bellBoxImg = document.getElementById("navbar-bell-icon"); // img
+
+    alertBoxDiv.innerHTML = `확인하지 않은 알림이 ${notiCount}건 있습니다.`;
+    bellBoxImg.src = "/icon/layout/bell_on_dark.svg";
+})
