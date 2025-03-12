ca7ac32e4fbb1f73ee4fcefabeef15bdf0558560
Queue-ri <qriositylog@gmail.com>
2025-03-12 17:09:49 +0900

    feat: 운영자 페이지 팝업스토어 정보 UI 추가 (#80)

--- common-service/src/main/resources/static/js/manager_store_info.js
+++ common-service/src/main/resources/static/js/manager_store_info.js
@@ -1,2 +1,17 @@
+const API_GATEWAY = "http://localhost:9093";
+
 // 접속한 MANAGER의 memberCode 확인
 console.log('memberCode:', memberCode);
+
+
+function handleImageBoxClicked(event) {
+    // img src 가져오기
+    const imageLink = event.firstChild.src;
+    console.log("clicked image link:", imageLink);
+    window.open(imageLink, '_blank');
+}
+
+function handlePreviewButtonClicked(sid) {
+    // memberCode는 전역에서 접근 가능
+    window.open(API_GATEWAY + "/store?id=" + sid, '_blank');
+}
