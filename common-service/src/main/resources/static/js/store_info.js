3b6e9c9b8d3e2ba8951fc931ab3f3da4b4439fa2
Queue-ri <qriositylog@gmail.com>
2025-03-14 10:50:14 +0900

    feat: 팝업스토어 상세 페이지 UI 추가 (#49)
    미리보기 모드 반영 완료

--- common-service/src/main/resources/static/js/store_info.js
+++ common-service/src/main/resources/static/js/store_info.js
@@ -1,3 +1,37 @@
-function goto_reservation_page(id) {
-    location.href = `http://localhost:9093/reserve?id=${id}`;
+const API_GATEWAY = "http://localhost:9093";
+
+// 접속한 사용자의 memberCode 확인
+console.log('memberCode:', memberCode);
+
+
+function handleImageBoxClicked(event) {
+    // img src 가져오기
+    const imageLink = event.firstChild.src;
+    console.log("clicked image link:", imageLink);
+    window.open(imageLink, '_blank');
+}
+
+/* '목록으로' 버튼 클릭시 작동하는 함수 */
+function goBack() {
+    if (memberRole === 'MANAGER') {
+        alert("미리보기 모드입니다.");
+        return;
+    }
+    history.back();
+}
+
+/* [이용자] '예약하기' 버튼 클릭시 작동하는 함수 */
+function gotoReservationPage(storeId) {
+    location.href = API_GATEWAY + "/reserve?id=" + storeId;
+}
+
+/* 비로그인 상태에서 예약하기 버튼 클릭시 작동하는 함수 */
+function askSignIn() {
+    const ok = confirm("로그인이 필요한 서비스입니다.\n로그인하시겠습니까?");
+    if (ok) showOverlay(); // layout-member.js의 로그인 오버레이 함수 호출
+}
+
+/* [운영자] 미리보기 모드에서 예약하기 버튼 클릭시 작동하는 함수 */
+function alertPreview() {
+    alert("미리보기 모드입니다.");
 }
